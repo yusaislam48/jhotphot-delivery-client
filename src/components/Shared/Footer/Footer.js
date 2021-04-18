920d6ca940989fd1675758d202d39e42ae22791f
Yusa Islam <yusaislam48@gmail.com>
2021-04-18 20:13:54 +0600

    updated footer

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -6,42 +6,31 @@ import FooterCol from './FooterCol/FooterCol';
 
 const Footer = () => {
     const noNamed = [
-        {name: "Emergency Dental Care" , link: "/emergency"},
-        {name: "Check Up" , link: "/checkup"},
-        {name: "Treatment of Personal Diseases" , link: "/personal-treatment"},
-        {name: "Tooth Extraction" , link: "/tooth-extract"},
-        {name: "Check Up" , link: "/checkup"},
+        {name: "Home" , link: "/"},
+        {name: "Services" , link: "#"},
+        {name: "Fees" , link: "#"}
     ]
     const ourAddress = [
-        {name: "New York - 101010 Hudson" , link: "//google.com/map"},
-        {name: "Yards" , link: "//google.com/map"},
+        {name: "Rahman Nagar, Bogura" , link: "//google.com/map"}
        
     ]
-    const oralHealth = [
-        {name: "Emergency Dental Care" , link: "/emergency"},
-        {name: "Check Up" , link: "/checkup"},
-        {name: "Treatment of Personal Diseases" , link: "/personal-treatment"},
-        {name: "Tooth Extraction" , link: "/tooth-extract"},
-        {name: "Check Up" , link: "/checkup"},
-        {name: "Check Up" , link: "/checkup"},
-        {name: "Check Up" , link: "/checkup"}
+    const ourServices = [
+        {name: "Home Delivery" , link: "#"},
+        {name: "Pickup From Home" , link: "#"},
+        {name: "Food Delivey" , link: "#"}
     ]
-    const services = [
-        {name: "Emergency Dental Care" , link: "/emergency"},
-        {name: "Check Up" , link: "/checkup"},
-        {name: "Treatment of Personal Diseases" , link: "/personal-treatment"},
-        {name: "Tooth Extraction" , link: "/tooth-extract"},
-        {name: "Check Up" , link: "/checkup"},
-        {name: "Check Up" , link: "/checkup"},
-        {name: "Check Up" , link: "/checkup"}
+    const Client = [
+        {name: "Book A Service" , link: "#"},
+        {name: "Booking List" , link: "#"},
+        {name: "Place a review" , link: "#"}
     ]
     return (
         <footer className="footer-area clear-both">
             <div className="container pt-5">
                 <div className="row py-5">
                     <FooterCol key={1} menuTitle={"."} menuItems={noNamed}/>
-                    <FooterCol key={2} menuTitle="Services" menuItems={services}/>
-                    <FooterCol key={3} menuTitle="Oral Health" menuItems={oralHealth}/>
+                    <FooterCol key={2} menuTitle="Client" menuItems={Client}/>
+                    <FooterCol key={3} menuTitle="Services" menuItems={ourServices}/>
                     <FooterCol key={4} menuTitle="Our Address" menuItems={ourAddress}> 
                         <ul className="social-media list-inline">
                             <li className="list-inline-item"><a href="//facebook.com"><FontAwesomeIcon className="icon active-icon" icon={faFacebookF} /></a></li>
