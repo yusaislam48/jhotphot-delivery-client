0a30df5a0bdba3288e9f174e3bafe576df9146f0
Yusa Islam <yusaislam48@gmail.com>
2021-04-18 19:58:10 +0600

    added custom service counter

--- src/components/Home/Counter/Counter.js
+++ src/components/Home/Counter/Counter.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Counter.css'
 
 const Counter = () => {
+    const [services, setServices] = useState([]);
+    useEffect(()=>{
+        fetch('https://jhotphodelivery.herokuapp.com/services')
+        .then(res => res.json())
+        .then(data => setServices(data))
+    }, [services]);
+
     return (
         <div className="container mt-5 mb-5">
             <h2 className='text-center section-header pb-4'>AT A <span className='h-text-color'>GLANCE</span></h2>
@@ -13,18 +20,18 @@ const Counter = () => {
                     </div>
                 </div>
                 <div className="four col-md-3 mb-3">
-                    <div className="counter-box"> <i className="fa fa-group"></i> <span className="counter">3275</span>
+                    <div className="counter-box"> <i className="fa fa-group"></i> <span className="counter">325</span>
                         <p>Registered Members</p>
                     </div>
                 </div>
                 <div className="four col-md-3 mb-3">
-                    <div className="counter-box"> <i className="fa fa-shopping-cart"></i> <span className="counter">289</span>
-                        <p>Available Products</p>
+                    <div className="counter-box"> <i className="fa fa-shopping-cart"></i> <span className="counter">{services.length}</span>
+                        <p>Available Services</p>
                     </div>
                 </div>
                 <div className="four col-md-3 mb-3">
-                    <div className="counter-box"> <i className="fa fa-user"></i> <span className="counter">1563</span>
-                        <p>Saved Trees</p>
+                    <div className="counter-box"> <i className="fa fa-user"></i> <span className="counter">150</span>
+                        <p>Total Rider</p>
                     </div>
                 </div>
             </div>
